6d6c2cc3a7473aa4933b11153b2053667ab7a1e0
Bhanu581946 <bhanupratapcto@gmail.com>
2025-10-04 13:50:52 +0530

    Example of Documentation String

--- P46.py
+++ P46.py
@@ -1,6 +1,6 @@
 import prime_module
 num = 30
 if prime_module.is_prime(num):
-    print("{num} is a prime number.")
+    print(f"{num} is a prime number.")
 else:
-    print("{num} is not a prime number.")
+    print(f"{num} is not a prime number.")
