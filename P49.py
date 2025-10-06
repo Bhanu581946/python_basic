2e4f50f0d80b159d8ec0ce1068ca90edb43e98cb
Bhanu581946 <bhanupratapcto@gmail.com>
2025-10-06 12:41:45 +0530

    example of decorators

--- P49.py
+++ P49.py
@@ -1,8 +1,8 @@
 def decorator(f):
     def wrapper():
         print("Something is happening before the function is called.")
-        print("Something is happening after the function is called.")
         f()
+        print("Something is happening after the function is called.")
     return wrapper
 @decorator
 def greet():
