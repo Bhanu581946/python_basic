2bea749ad80b8510a60385588c4eefa431fb0b10
Bhanu581946 <bhanupratapcto@gmail.com>
2025-10-06 14:01:37 +0530

    func to a variable

--- P51.py
+++ P51.py
@@ -1,10 +1,13 @@
-def decor(fun):
-    def inner():
-        value= fun()
+def decor(fun): #r3
+    def inner(): #r2
+        value= fun() # r1
         return value+2
-    return inner
-# @ decor
+    return inner # r4
+
+
+# @decor
 def num():
     return 10
+
 result_f=decor(num)
 print(result_f())
