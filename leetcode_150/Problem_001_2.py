b9bd557ac4cd55b8bedd6701445fac5ff99ece93
Bhanu581946 <bhanupratapcto@gmail.com>
2025-10-08 11:54:36 +0530

    Two Sum - Two Pass Hash Map Solution

---
+++ leetcode_150/Problem_001_2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums, target):
+        numMap = {}
+        n = len(nums)
+
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+obj1 = Solution()
+print(obj1.twoSum([2,7,11,15],9)) # No solution found
